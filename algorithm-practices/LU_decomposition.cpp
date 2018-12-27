7efaa91a2e8409aecd45e9d59655e9bef17ecd60
Christopher <isunduda@gmail.com>
2018-12-27 22:30:45 +0800

    Initialise

---
+++ algorithm-practices/LU_decomposition.cpp
@@ -0,0 +1,16 @@
+/*  Linear Algebra Coding
+    LU decomposition    */
+
+#include "pch.h"
+#include <iostream>
+#include <boost/numeric/ublas/matrix.hpp>
+
+using namespace std;
+
+int testfun(){
+    string out = "Hello World!\r\n";
+    //matrix a;
+    cout<<out<<endl;
+    system("PAUSE");
+    return 0;
+}
